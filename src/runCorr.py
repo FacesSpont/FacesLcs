39221b0af08145c551c16339b718da9fe815afcd
Vitor Peres <vitor.peres@hygiabank.com>
2022-10-16 23:51:04 -0300

    notes correlation

--- src/runCorr.py
+++ src/runCorr.py
@@ -15,14 +15,13 @@ for ent in entries:
     nome = ent.split('.')
     if 'analysis' in ent:
 
-        print(ent)
         data = pd.read_csv(path + ent, delimiter=';')
 
         data = data.iloc[:, 1:18]
 
         data.corr()
 
-        ax = sns.heatmap(data.corr(), cmap=sns.diverging_palette(20, 220, n=200),square=True);
+        ax = sns.heatmap(data.corr(), cmap=sns.diverging_palette(20, 220, n=200),square=True, annot=True);
 
         ax.set_xticklabels(
             ax.get_xticklabels(),
@@ -33,7 +32,7 @@ for ent in entries:
         plt.gcf().set_size_inches(11.7, 8.27)
         plt.savefig("./extras/correlations/"+nome[0]+".png",bbox_inches='tight',dpi=100)
         
-        plt.show()
+        # plt.show()
         
         plt.clf()
         plt.close()
